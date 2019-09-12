af32c0775f872b36daf39391685ee19a3ab3f377
Arman <riasiarman@yahoo.com>
2019-09-12 19:23:48 +0430

    Adding empty

---
+++ Solo Mission/Solo Mission/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Solo Mission
+//
+//  Created by Arman Riasi on 9/12/19.
+//  Copyright © 2019 Arman Riasi. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+   
+}
---
+++ Solo Mission/Solo Mission/GameViewController.swift
@@ -0,0 +1,50 @@
+//
+//  GameViewController.swift
+//  Solo Mission
+//
+//  Created by Arman Riasi on 9/12/19.
+//  Copyright © 2019 Arman Riasi. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            let scene = GameScene(size: CGSize(width: 1536, height: 2048))
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
