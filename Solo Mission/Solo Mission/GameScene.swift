004f6b1693b77322d5d56c32e679535140471a21
Arman <riasiarman@yahoo.com>
2019-09-12 19:44:15 +0430

    Fixing margins and playable width

--- Solo Mission/Solo Mission/GameScene.swift
+++ Solo Mission/Solo Mission/GameScene.swift
@@ -14,6 +14,24 @@ class GameScene: SKScene {
    
     let player = SKSpriteNode(imageNamed: "playerShip")
     
+    //    define gameArea a variable of type CGRect
+    var gameArea: CGRect
+    
+    override init(size: CGSize) {
+        
+        let maxAspectRatio: CGFloat = (size.height / size.width) + 0.80
+        let playableWidth = size.height/maxAspectRatio
+        let margin = (size.width - playableWidth) / 2
+        gameArea = CGRect(x: margin, y: 0, width: playableWidth, height: size.height)
+        
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.size = self.size
@@ -51,6 +69,14 @@ class GameScene: SKScene {
             let previousPointOfTouch = touch.previousLocation(in: self)
             let amountDragged = pointOfTouch.x - previousPointOfTouch.x
             player.position.x += amountDragged
+            
+            if player.position.x > gameArea.maxX - player.size.width / 2 {
+                player.position.x = gameArea.maxX - player.size.width / 2
+            }
+            if player.position.x < gameArea.minX + player.size.width / 2 {
+                player.position.x = gameArea.minX + player.size.width / 2
+            }
+        
         }
     }
 }
